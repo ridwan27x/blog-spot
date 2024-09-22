63e17fe08de35f8875979882acf6e051389d696f
fajar ilham <Yutabot59@gamil.com>
2024-09-22 20:39:14 +0700

    memodifikasi tampilan showblade2

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -20,6 +20,7 @@
     </div>
     <a href="{{ route('home') }}" class="btn btn-secondary mt-3">
         <i class="bi bi-arrow-left"></i> Kembali ke Daftar Postingan
+        <i class="bi bi-arrow-left"></i> hallo
     </a>
 </div>
 @endsection
